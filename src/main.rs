a171106739e3559675a16de83b855bb50023a855
JZims <zimmerman.j@hotmail.com>
2024-01-03 13:33:46 -0500

    flap and velocity added

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ struct State {
 
 impl Player {
 
-    fn new(X:i32, y:i32) -> Self {
+    fn new(x:i32, y:i32) -> Self {
         Player {
             x, 
             y,
@@ -33,6 +33,21 @@ impl Player {
             BLACK,
             to_cp437('@')
         );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0{
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap (&mut self) {
+        self.velocity = -2.0;
     }
 }
 
