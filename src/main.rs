a8a6b75a521fbdc74fe8f82a2888dbd33b8e52b2
JZims <zimmerman.j@hotmail.com>
2024-01-04 14:35:34 -0500

    player actions finished

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+
 struct Player {
     x:i32,
     y:i32, 
@@ -13,6 +18,8 @@ struct Player {
 }
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
@@ -22,6 +29,7 @@ impl Player {
         Player {
             x, 
             y,
+            velocity: 0.0,
         }
     }
 
@@ -55,16 +63,38 @@ impl State {
 
     fn new() -> Self {
         State {
+            player: Player::new(5,25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
     
     fn play (&mut self, ctx: &mut BTerm){
-        //TODO: Fill in this stub later
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+
+        ctx.print(0,0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
+
+       
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5,25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
